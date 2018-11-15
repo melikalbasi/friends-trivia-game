41332ff07234962d27434935f89af0479c3c936f
Melika Kalbasi <melika_kalbasi@melikas-air.gateway.sonic.net>
2018-11-14 19:16:20 -0800

    html and psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+// declare global variables
+    // startBtn variable
+    // quizContainer variable
+    // submitBtn variable
+    // results variable
+    // variable for myQuestions (make questions into objects with correctAnswer in the key)
+    // variable for correctAnswer
+    // variable for userInput
+    // variable for output
+
+// use a function for buildQuiz to initialize game to display features and have it ready for reset
+    // display start button
+
+    // display timer to start at 120 seconds
+    // display all questions and answer selections
+    // display done button
+
+    // display for all done!
+    //display for correct answers
+    //display for incorrect answers
+    //display for unanswered questions
+
+// display quiz right awat buildQuiz();
+
+
+
+//declare a function to showResults
+    // on submit show the results
+        // add results to correct answers
+        // add results to incorrect answers
+        // add results to unanswered questions
+
+    //on timeout show the results
+        // add results to correct answers
+        // add results to incorrect answers
+        // add results to unanswered questions
+
+//on submit show results submitBtn.addEventListener("click", showResults);
+
+
+    
+
+
+
+
+
+
+
+
+})
+
+
+
+
